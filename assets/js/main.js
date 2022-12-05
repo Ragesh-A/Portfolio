cb32859f15b0ae749461ad6f80f374dd6fe24ab0
Ragesh-A <ragesha2017@gmail.com>
2022-12-05 09:07:51 +0530

    form validation

--- assets/js/main.js
+++ assets/js/main.js
@@ -259,3 +259,21 @@
   new PureCounter();
 
 })()
+
+$("#submit-form").submit((e)=>{
+  e.preventDefault()
+  $.ajax({
+      url:"https://script.google.com/macros/s/AKfycbw4QipLDuPbjxjeR8NtA14t8OlNCdFaMXLFKpKc6URvFsUi4GWlC8Yo3ZlNWcwaGmiq9w/exec",
+      data:$("#submit-form").serialize(),
+      method:"post",
+      success:function (response){
+          alert("Form submitted successfully")
+          window.location.reload()
+          //window.location.href="https://google.com"
+      },
+      error:function (err){
+          alert("Something Error")
+
+      }
+  })
+})
